6b0630462ffc2860d67ea279878b1adfb5bacf7c
BayNguyen <baynguyen@pvs.com.vn>
2023-01-30 21:19:44 +0700

    test-swagger-laravel

--- app/Http/Controllers/API/ToppageController.php
+++ app/Http/Controllers/API/ToppageController.php
@@ -13,6 +13,28 @@ class ToppageController extends Controller
         
     }
 
+    /** 
+        * @OA\Get( 
+            * path="/api/toppage",
+            * summary="Get data for Top page",
+            * operationId="Toppage Api",
+            * tags={"top-page"},
+            * security={{"sanctum":{}}},
+            
+            * @OA\Response(
+                * response=200,
+                * description="Success response",
+                * @OA\JsonContent( 
+                    * @OA\Property(property="success", type="boolean", example=true),
+                    * @OA\Property(property="message", type="string", example=""),
+                    * @OA\Property(property="status_code", type="integer", example=200),
+                    * @OA\Property(property="data", type="object", example={
+                        
+                    * }) 
+                * )
+            * ) 
+        * ) 
+    */
     public function index(Request $request)
     {
         $user = auth('api')->user();
